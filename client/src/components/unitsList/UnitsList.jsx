62602fdddd125f111a9a89388e3460fe587dbd80
Nayeli Andrade <91092346+NayeliAndrade@users.noreply.github.com>
2024-01-16 23:27:31 +0000

    Update styles

--- client/src/components/unitsList/UnitsList.jsx
+++ client/src/components/unitsList/UnitsList.jsx
@@ -11,6 +11,8 @@ const UnitsList = () => {
     return (
         //Componente de las unidades
         <section className="units-list">
+            <h2 className="units-list__title">Presione la carta de una unidad</h2>
+            <p className="units-list__paragraph">Podrás ver su último evento</p>
             {dataUnits.map((unit) => (
                 <article className="card" onClick={() => handleClick(unit)} key={unit.IDGPS}>
                     <header>
--- client/src/sections/Header.jsx
+++ client/src/sections/Header.jsx
@@ -1,16 +1,23 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+    const menu = useRef(null);
+
+    const handleClickMenu = () => {
+        menu.current.classList.toggle("menu--open");
+    }
     return (
         <header className="header">
             <nav className="header__nav">
                 <h1 className="header__logo">Tecnocontrol</h1>
-                <ul className="list">
+                <button type="button" title="open-menu" onClick={handleClickMenu} className="menu__icon-container"><div className="menu__icon"></div></button>
+                <ul ref={menu} className="menu">
                     <li>
-                        <Link className="list__link" to={"/"}>Unidades</Link>
+                        <Link className="menu__link" to={"/"}>Unidades</Link>
                     </li>
                     <li>
-                        <Link className="list__link" to={"/reports"}>Reportes</Link>
+                        <Link className="menu__link" to={"/reports"}>Reportes</Link>
                     </li>
                 </ul>
             </nav>
